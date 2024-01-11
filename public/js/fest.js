9501916d10cf1cecd99812ff8dcf654072b2b03a
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2024-01-11 16:49:33 -0500

    base wireframe

--- public/js/fest.js
+++ public/js/fest.js
@@ -1,15 +1,49 @@
 /* onclick to each button to pull up embedded video - do i need to make separate onclick functions. */
+const tooltips = document.querySelectorAll('.all-tooltips .tooltip')
+const container = document.querySelector('.container')
+let timeoutId
 
 function dGlover() {
     const videoPlayer = document.getElementById("dials")
     videoPlayer.innerHTML = `<iframe src="https://www.youtube.com/embed/VYOjWnS4cMY?autoplay=1&mute=1">`  
 }
 
+function contentPosition(){
+    tooltips.forEach(tooltip => {
+        const entry = tooltip.querySelector('.title')
+        const content = document.getElementById('tooltip-content')
+        content.style.left = entry.offsetLeft  + 'px'
+    })
+}
+
+contentPosition()
+
+tooltips.forEach(tooltip =>{
+    const entry = tooltip.querySelector('title')
+    const content = tooltip.querySelector('.tooltip-content')
+    entry.addEventListener('mousemove', () => {
+        tooltip.classList.add('active');
+    })
+    entry.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            tooltip.classList.remove('active')
+        } ,100)
+    })
+    content.addEventListener('mousemove', () => {
+        clearTimeout(timeoutId)
+        tooltip.classList.add('active');
+    })
+    content.addEventListener('mouseleave', () => {
+        timeoutId = setTimeout(() => {
+            tooltip.classList.remove('active')
+        } ,100)
+})
+})
 
 /* Artists (Headliners)
 1. Childish Gambino 
 2. durand jones
-3. Jungle
+3. Leon Bridges
 4. Moonchild 
 5. H.E.R
 6. Durand Bernarr
@@ -18,21 +52,21 @@ function dGlover() {
 9. The Internet
 10. BJ the Chicago Kid
 11. The Seratones
-12. Nicolay
+12. The Coolots
 
 
 (Local)
 1. Foxbat
 2. Dizzy Dreamer 
-3. Quon Solo 
+3. Poet series
 4. DJ Alli 
-5. Deep Sea Rice 
+5. DJ Bombshell
 6. Rythym Science Sound 
 7. Salem Vytch Tryells
 8. Va Va Vixens
 9. Kiana and the Sun Kings 
 10. The Jesse Lees
-11. Kimmiee
+11. Open mic hour
 12. Dj doro & Dj Exklusive
 
 */
